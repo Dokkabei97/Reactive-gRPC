98e4e8b117728a2f02e252950e40f6bf4e408414
Dokkabei97 <wkdrn970@naver.com>
2025-04-27 22:02:52 +0900

    Feat: implement bulk create, update, and delete operations for products in GrpcServerService

--- pmp/src/main/kotlin/com/hl/pmp/domain/GrpcServerService.kt
+++ pmp/src/main/kotlin/com/hl/pmp/domain/GrpcServerService.kt
@@ -1,8 +1,13 @@
 package com.hl.pmp.domain
 
+import org.springframework.grpc.product.proto.CreateProductBulkRequest
 import org.springframework.grpc.product.proto.CreateProductRequest
+import org.springframework.grpc.product.proto.DeleteProductBulkRequest
+import org.springframework.grpc.product.proto.DeleteProductRequest
 import org.springframework.grpc.product.proto.ProductResponse
 import org.springframework.grpc.product.proto.ProductServiceGrpcKt
+import org.springframework.grpc.product.proto.UpdateProductBulkRequest
+import org.springframework.grpc.product.proto.UpdateProductRequest
 import org.springframework.stereotype.Service
 
 @Service
@@ -22,4 +27,40 @@ class GrpcServerService(
             .setMessage("Product created")
             .build()
     }
+
+    override suspend fun createProductsBulk(request: CreateProductBulkRequest): ProductResponse {
+        val products =
+            request.productsList.map {
+                Product(
+                    it.name,
+                    it.price,
+                )
+            }
+        productService.createBulk(products)
+
+        return ProductResponse
+            .newBuilder()
+            .setMessage("Products created")
+            .build()
+    }
+
+    override suspend fun updateProduct(request: UpdateProductRequest): ProductResponse {
+        val product =
+            Product(
+                request.product.name,
+                request.product.price,
+            )
+        productService.update(product)
+
+        return ProductResponse
+            .newBuilder()
+            .setMessage("Product updated")
+            .build()
+    }
+
+    override suspend fun updateProductsBulk(request: UpdateProductBulkRequest): ProductResponse = super.updateProductsBulk(request)
+
+    override suspend fun deleteProduct(request: DeleteProductRequest): ProductResponse = super.deleteProduct(request)
+
+    override suspend fun deleteProductsBulk(request: DeleteProductBulkRequest): ProductResponse = super.deleteProductsBulk(request)
 }
